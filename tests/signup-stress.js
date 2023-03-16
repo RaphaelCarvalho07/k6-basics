998a870a3abb0ca180f64bf1d69694a9cbea73e0
RaphaelCarvalho07 <raphael.carvalho.qa@gmail.com>
2023-03-16 18:30:35 -0300

    implementation of load testing

--- tests/signup-stress.js
+++ tests/signup-stress.js
@@ -11,8 +11,17 @@ export function handleSummary(data) {
 }
 
 export const options = {
-    vus: 1,
-    duration: '1m',
+    stages: [
+        { duration: "2m", target: 100 }, // below normal load
+        { duration: "5m", target: 100 },
+        { duration: "2m", target: 200 }, // normal load
+        { duration: "5m", target: 200 },
+        { duration: "2m", target: 300 }, // around the breaking point
+        { duration: "5m", target: 300 },
+        { duration: "2m", target: 400 }, // beyond the breaking point
+        { duration: "5m", target: 400 },
+        { duration: "10m", target: 0 }, // scale down. Recovery stage.
+    ],
     thresholds: {
         http_req_duration: ['p(95)<2000'], // 95% das requisições devem responder em até 2s
         http_req_failed: ['rate<0.01'] // Apenas 1% das requisições podem apresentar algum tipo de erro  
