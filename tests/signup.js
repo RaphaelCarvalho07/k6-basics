2a841bb24395be40ff1525d582abf93ae5b49f5d
RaphaelCarvalho07 <raphael.carvalho.qa@gmail.com>
2023-03-16 16:15:15 -0300

    removing api timeout / including some thresholds for the requisitions

--- tests/signup.js
+++ tests/signup.js
@@ -4,7 +4,11 @@ import uuid from './libs/uuid.js'
 
 export const options = {
     vus: 10,
-    duration: '30s'
+    duration: '30s',
+    thresholds: {
+        http_req_duration: ['p(95)<2000'], // 95% das requisições devem responder em até 2s
+        http_req_failed: ['rate<0.01'] // Apenas 1% das requisições podem apresentar algum tipo de erro  
+    }
 }
 
 export default function () {
