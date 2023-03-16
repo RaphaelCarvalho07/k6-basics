aac0cca64d0b7941f8c05c4a7140efab0505a295
RaphaelCarvalho07 <raphael.carvalho.qa@gmail.com>
2023-03-16 16:27:50 -0300

    impletation smoke test

---
+++ tests/signup-smoke.js
@@ -0,0 +1,36 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import uuid from './libs/uuid.js'
+
+export const options = {
+    vus: 1,
+    duration: '1m',
+    thresholds: {
+        http_req_duration: ['p(95)<2000'], // 95% das requisições devem responder em até 2s
+        http_req_failed: ['rate<0.01'] // Apenas 1% das requisições podem apresentar algum tipo de erro  
+    }
+}
+
+export default function () {
+    const URL = 'http://localhost:3333/signup'
+
+    const PAYLOAD = JSON.stringify(
+        {
+            email: `${uuid.v4().substring(24)}@qa.raphilskecompany.com`,
+            password: 'pwd123'
+        })
+
+    const HEADERS = {
+        'headers': {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    const RES = http.post(URL, PAYLOAD, HEADERS)
+
+    check(RES, {
+        'status code should be 201': (r) => r.status === 201
+    })
+
+    sleep(1)
+}
